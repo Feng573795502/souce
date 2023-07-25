a32ce517edace4d30cd3bcc99a6719dd670288e7
Feng573795502 <573795502@qq.com>
2023-07-25 19:06:37 +0800

    网口添加部分功能

--- 24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/xsim_webtalk.tcl
+++ 24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Jul 24 15:24:01 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Jul 25 11:42:56 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ce9f5905-8617-46e0-af73-2f426adc795f" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "ef0cb65ac85a461ca37dae84893af189" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 7 3800X 8-Core Processor             " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "689 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6892_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3267163972 -regid "" -xml H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6868_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1956398283 -regid "" -xml H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm H:/FPGA/souce/24_eth_udp/eth_udp.sim/sim_1/behav/xsim/xsim.dir/crc32_d8_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
